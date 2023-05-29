dac43e0d05161e5c64affc39c17eca591ee988d2
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-05-29 04:20:12 +0300

    🎉 feat(game): add rock-paper-scissors game typescript implementation

---
+++ README.md
@@ -0,0 +1,21 @@
+1) ссылку на видео, демонстрирующее запуск
+2) с 3 и 7 параметрами,
+3) запуск с неправильными параметрами (повторение или чётное число, один или отсутствие),
+4) генерацию таблицы помощи (на 5 параметрах),
+5) выбор хода,
+6) вывод результатов;
+7) исходный код ссылкой на гитхаб.
+
+`node .\dist\index.js rock paper scissors`
+
+`node .\dist\index.js 1 2 3 4 5 6 7`
+
+`node .\dist\index.js 1 2`
+
+`node .\dist\index.js 1 1 1`
+
+`node .\dist\index.js 1`
+
+`node .\dist\index.js`
+
+`node .\dist\index.js rock paper scissors lizard Spock`
--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,141 @@
-const readline = require('readline');
-const crypto = require('crypto');
+import { createInterface } from 'readline';
+import { randomBytes, createHmac } from 'crypto';
 
-class Generator {
-    generateKey() {
-        return crypto.randomBytes(32).toString('hex');
+const CliTable = require('cli-table');
+
+enum Results {
+    Draw,
+    Win,
+    Lose
+}
+
+interface GeneratorInterface {
+    generateKey(): string;
+
+    generateHmac(key: string, data: string): string;
+}
+
+interface RulesInterface {
+    isValidMove(move: string | number): boolean;
+
+    determineWinner(userMove: string, computerMove: string): number;
+}
+
+interface TableInterface {
+    printTable(): void;
+}
+
+interface GameInterface {
+    playGame(): Promise<void>;
+
+    displayMenu(): void;
+
+    displayResult(): void;
+
+    displayHelp(): void;
+}
+
+class Generator implements GeneratorInterface {
+    generateKey(): string {
+        return randomBytes(32).toString('hex');
     }
 
-    generateHmac(key, data) {
-        const hmac = crypto.createHmac('sha256', key);
+    generateHmac(key: string, data: string): string {
+        const hmac = createHmac('sha256', key);
         hmac.update(data);
         return hmac.digest('hex');
     }
 }
 
-class Rules {
-    constructor(moves) {
+class Rules implements RulesInterface {
+    private moves: string[];
+
+    constructor(moves: string[]) {
         this.moves = moves;
     }
 
-    isValidMove(move) {
-        const moveIndex = parseInt(move);
+    isValidMove(move: string | number): boolean {
+        const moveIndex = parseInt(move.toString());
         return this.isNumeric(moveIndex) && this.isWithinRange(moveIndex);
     }
 
-    isNumeric(value) {
+    private isNumeric(value: number): boolean {
         return !isNaN(value);
     }
 
-    isWithinRange(value) {
+    private isWithinRange(value: number): boolean {
         return value >= 1 && value <= this.moves.length;
     }
 
-    determineWinner(userMove, computerMove) {
+    determineWinner(userMove: string, computerMove: string): number {
         const userIndex = this.moves.indexOf(userMove);
         const computerIndex = this.moves.indexOf(computerMove);
         const length = this.moves.length;
         switch (computerIndex) {
             case userIndex:
-                return 0;
+                return Results.Draw;
             case (userIndex + 1) % length:
             case (userIndex + ~~(length / 2)) % length:
-                return 2;
+                return Results.Lose;
             default:
-                return 1;
+                return Results.Win;
         }
     }
 }
 
-class Table {
-    constructor(moves, rules) {
+class Table implements TableInterface {
+    private readonly results: string[];
+    private readonly moves: string[];
+    private rules: RulesInterface;
+    private readonly table: string[][];
+
+    constructor(moves: string[], rules: RulesInterface) {
         this.results = ['Draw', 'Win', 'Lose'];
         this.moves = moves;
         this.rules = rules;
         this.table = this.generateTable();
     }
 
-    generateTable() {
-        const table = [['Moves', ...this.moves]];
-        const rows = this.moves.reduce((acc, userMove) => {
-            const row = this.generateRow(userMove);
-            acc.push(row);
-            return acc;
-        }, []);
-        table.push(...rows);
+    private generateTable(): string[][] {
+        const table = new CliTable();
+        table.push(['Moves', ...this.moves]);
+        this.moves.forEach((userMove: string) => {
+            const row: string[] = this.generateRow(userMove);
+            table.push(row);
+        });
         return table;
     }
 
-    generateRow(userMove) {
-        const row = [userMove];
-        const cells = this.moves.map(computerMove => this.generateCell(userMove, computerMove));
+    private generateRow(userMove: string): string[] {
+        const row: string[] = [userMove];
+        const cells: string[] = this.moves.map((computerMove: string) => this.generateCell(userMove, computerMove));
         row.push(...cells);
         return row;
     }
 
-    generateCell(userMove, computerMove) {
+    private generateCell(userMove: string, computerMove: string): string {
         const winner = this.rules.determineWinner(userMove, computerMove);
         return this.results[winner];
     }
 
-    printTable() {
-        console.table(this.table);
+    printTable(): void {
+        console.log(this.table.toString());
     }
 }
 
-class Game {
-    constructor(moves, generator, rules, table) {
+class Game implements GameInterface {
+    private readonly results: string[];
+    moves: string[];
+    generator: GeneratorInterface;
+    rules: RulesInterface;
+    table: TableInterface;
+    private key: string;
+    private hmac: string;
+    private computerMove: string;
+    private userMove: string;
+    result: string;
+
+    constructor(moves: string[], generator: GeneratorInterface, rules: RulesInterface, table: TableInterface) {
         this.results = ['Draw!', 'You Win!', 'You Lose!'];
         this.moves = moves;
         this.generator = generator;
@@ -92,36 +143,36 @@ class Game {
         this.table = table;
     }
 
-    getRandomMove() {
+    private getRandomMove(): number {
         return ~~(Math.random() * this.moves.length);
     }
 
-    generateComputerMove() {
+    private generateComputerMove(): void {
         this.key = this.generator.generateKey();
         this.computerMove = this.moves[this.getRandomMove()];
         this.hmac = this.generator.generateHmac(this.key, this.computerMove);
     }
 
-    async getUserInput() {
-        const rl = readline.createInterface({
+    private async getUserInput(): Promise<string> {
+        const rl = createInterface({
             input: process.stdin,
             output: process.stdout
         });
         return new Promise((resolve) => {
-            rl.question('Enter your move: ', (input) => {
+            rl.question('Enter your move: ', (input: string) => {
                 rl.close();
                 resolve(input);
             });
         });
     }
 
-    play(userMoveIndex) {
+    private play(userMoveIndex: number): void {
         this.userMove = this.moves[userMoveIndex - 1];
         const winner = this.rules.determineWinner(this.userMove, this.computerMove);
         this.result = this.results[winner];
     }
 
-    async playGame() {
+    async playGame(): Promise<void> {
         this.generateComputerMove();
         while (true) {
             this.displayMenu();
@@ -146,17 +197,17 @@ class Game {
         await this.playGame();
     }
 
-    displayMenu() {
+    displayMenu(): void {
         console.log(`HMAC: ${this.hmac}`);
         console.log('Available moves:');
-        this.moves.forEach((move, index) => {
+        this.moves.forEach((move: string, index: number) => {
             console.log(`${index + 1} - ${move}`);
         });
         console.log('0 - exit');
         console.log('? - help');
     }
 
-    displayResult() {
+    displayResult(): void {
         console.log(`Your move: ${this.userMove}`);
         console.log(`Computer move: ${this.computerMove}`);
         console.log(this.result);
@@ -164,7 +215,7 @@ class Game {
         console.log();
     }
 
-    displayHelp() {
+    displayHelp(): void {
         this.table.printTable();
     }
 }
